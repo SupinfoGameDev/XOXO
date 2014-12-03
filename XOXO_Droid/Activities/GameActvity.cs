0bf8c3c9357f87736bdc844528412e09920b0765
aiemsal <iemsal.assous@gmail.com>
2014-12-03 20:06:11 +0100

    Activities not added in previous commit.
    Signed-off-by: aiemsal <iemsal.assous@gmail.com>

---
+++ XOXO_Droid/Activities/GameActvity.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XOXO_Droid
+{
+	[Activity (Label = "GameActivity")]			
+	public class GameActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Game);
+			Button case1 = FindViewById<Button> (Resource.Id.Button1);
+			case1.Click += (object sender, EventArgs e) => {
+				var drawable = GetDrawable(Resource.Drawable.cross);
+				case1.SetBackgroundDrawable(drawable);
+			};
+			// Create your application here
+		}
+		public override void OnBackPressed ()
+		{
+			//base.OnBackPressed ();
+		}
+	}
+}
+
---
+++ XOXO_Droid/Activities/SplashscreenActivity.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XOXO_Droid
+{
+	[Activity (Label = "XOXO", Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true, Icon="@drawable/xoxo")]			
+	public class SplashscreenActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			StartActivity(typeof(MenuActivity));
+			// Create your application here
+		}
+	}
+}
+
